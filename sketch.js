af48823ba530e98356d9b47ae741b735991bca18
ShrianshGoel <76607815+ShrianshGoel@users.noreply.github.com>
2021-09-10 19:19:56 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,20 @@
 
+var back,back1;
+
+
+function preload(){
+  back1=loadAnimation("giphy.gif");
+  
+}
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  back = createSprite(width/2,height/2,20,20);
+  back.addAnimation("screenshots",back1)
+}
+
+function draw() {
+  background(220);
+  drawSprites();
+}
